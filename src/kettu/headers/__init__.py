37fdbaf6283f907ffa3eeb512f3d84437e126aae
Souheil Chelfouh <trollfot@gmail.com>
2025-12-08 21:07:27 +0100

    Exposed http headers utils

--- src/kettu/headers/__init__.py
+++ src/kettu/headers/__init__.py
@@ -6,6 +6,9 @@ from .content_type import ContentType, MediaType, Accept
 from .language import Language, Languages
 from .etag import ETag, ETags
 from .link import Link, Links
+from .utils import parse_list_header, parse_header
+from .utils import parse_http_datetime, parse_host, parse_wsgi_path
+from .utils import encode_uri, serialize_http_datetime
 
 
 __all__ = [
@@ -16,5 +19,8 @@ __all__ = [
     "ContentType", "MediaType", "Accept",
     "Language", "Languages",
     "ETag", "ETags",
-    "Link", "Links"
+    "Link", "Links",
+    "parse_list_header", "parse_header",
+    "parse_http_datetime", "parse_host", "parse_wsgi_path",
+    "encode_uri", "serialize_http_datetime"
 ]
